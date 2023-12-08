61e5be9107bbd5fd4baa424d8ece495c3781aad4
Brandon Cano <brandoncano01@gmail.com>
2023-12-08 14:02:59 -0600

    added queries for storing user selected songs and better redis search

--- redisQueryTest.py
+++ redisQueryTest.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 # Connect to Redis
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -6,18 +7,33 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 # Test query this is the first song in the CSV file
 # FT.SEARCH songs_index '@name:(Testify)'
 index_name = 'songs_index'
-search_term = '@name:Born of a Broken Man'
-result = r.execute_command('FT.SEARCH', index_name, search_term)
+# this can be one query and whatever the search term ends up being can populate all three
+search_term = "Born of a Broken Man"
+search_query = f'@name:{search_term} | @artists:{search_term} | @album:{search_term}'
+result = r.execute_command('FT.SEARCH', index_name, search_query)
 for i in result: print(f"{type(i)}: {i}")
 
-# FT.SEARCH songs_index '@artists:Rage Against The Machine'
-search_term = '@artists:Rage Against The Machine'
-result = r.execute_command('FT.SEARCH', index_name, search_term)
+result = r.execute_command('FT.SEARCH', index_name, '@id:7lmeHLHBe4nmXzuXc0HDjk')
 for i in result: print(f"{type(i)}: {i}")
 
-# FT.SEARCH songs_index '@album:The Battle Of Los Angeles'
-search_term = '@album:The Battle Of Los Angeles'
-result = r.execute_command('FT.SEARCH', index_name, search_term)
-for i in result: print(f"{type(i)}: {i}")
+print('\n')
+# Storing user selected songs query, this will come from the next.js part
+session_id = "unique_idenifier_1"
+# The list of track_ids, will add to this list once a user selects a song:
+# FT.SEARCH songs_index "@id:7lmeHLHBe4nmXzuXc0HDjk"
+ids = ['7lmeHLHBe4nmXzuXc0HDjk', '1wsRitfRRtWyEapl0q22o8', '1hR0fIFK2qRG3f3RF70pb7']
+# create JSON payload to make Redis happy
+serialized_items = json.dumps(ids)
+r.hset(session_id, 'songs', serialized_items)
+serialized_items = r.hget(session_id, 'songs')
+id_list = []
+if serialized_items:
+    # if we get items then we need to deserialized the JSON to get the proper list
+    id_list = json.loads(serialized_items)
+    print(id_list)
 
-# [22, b'item:10561', [b'$', b'{"id":"3wmKa3pt9kyHKRxtqsVGYG","name":"Born of a Broken Man - Live at the Grand Olympic Auditorium, Los Angeles, CA - September 2000","artists":"[\'Rage Against The Machine\']","artist_ids":"[\'2d0hyoQ5ynDBnkvAbJKORj\']"}'], b'item:7401', [b'$', b'{"id":"3WmiisfwPICXyzJBrOSfI8","name":"Born Like This","artists":"[\'Three Days Grace\']","artist_ids":"[\'2xiIXseIJcq3nG7C8fHeBj\']"}'], b'item:6908', [b'$', b'{"id":"6lbGZSI5ptoB8JXn0wtBC2","name":"Born Under a Bad Sign","artists":"[\'Etta James\']","artist_ids":"[\'0iOVhN3tnSvgDbcg25JoJb\']"}'], b'item:1084', [b'$', b'{"id":"6YGPDRydgFqOB6UIIrYefx","name":"Before You Were Born","artists":"[\'Toad The Wet Sprocket\']","artist_ids":"[\'4j7EVY3kuDwLPfD2jfC7LC\']"}'], b'item:22282', [b'$', b'{"id":"5x3VjWoaGvUXFRrtr9frIr","name":"Maybe She\'s Born With It","artists":"[\'Stop It!!\']","artist_ids":"[\'4SZtuc01s2gmhy1njwuICj\']"}'], b'item:3268', [b'$', b'{"id":"3C8h5AnkJW2jrfJ7rcobsa","name":"Born to Hum","artists":"[\'Erin McKeown\']","artist_ids":"[\'715aftFykfb77AGFFs9WDj\']"}'], b'item:7547', [b'$', b'{"id":"3UUxgiU8x4pcBQm18Jp8z0","name":"Born Free (From The Film \\"Born Free\\")","artists":"[\'Kate Smith\', \'Glenn Osser\']","artist_ids":"[\'0Te1NusV6mkAMJ1VAkzJiX\', \'5IzlXnIwa5PWqv5Eh4vJnj\']"}'], b'item:22861', [b'$', b'{"id":"1hcMdkYr3oswSEPG4TGOe5","name":"With You I\'m Born Again","artists":"[\'Regine Velasquez\']","artist_ids":"[\'3wNylAjJX1PiCpMKx8Lrfp\']"}'], b'item:18798', [b'$', b'{"id":"6Kx7dhyOVXblnEs0AkJ4xI","name":"Babe Is Born/Enter The Stable Gently","artists":"[\'Liz Story\']","artist_ids":"[\'7yqRFgyP3seQwIu2FWGBbU\']"}'], b'item:6983', [b'$', b'{"id":"4HBxVkfgzZMS4m9AWpvOma","name":"I Was Born In The Country","artists":"[\\"Dan Melchior\'s Broke Revue\\"]","artist_ids":"[\'2FO7ma9WG3Zc4rg3KN5Lb8\']"}']]
+# Search for song info stored in index with the song id
+for id in id_list:
+    search_term = f'@id:{id}'
+    result = r.execute_command('FT.SEARCH', index_name, search_term)
+    print(result)
