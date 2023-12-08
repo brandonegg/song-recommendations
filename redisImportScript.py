84748fbec34523711cde784d6a3b7e4546b3a7b3
Brandon Cano <brandoncano01@gmail.com>
2023-12-08 13:27:18 -0600

    added ablum to the import query

--- redisImportScript.py
+++ redisImportScript.py
@@ -13,22 +13,27 @@ with open(PATH_TO_SONG_DATA, newline='', encoding='utf-8') as csvfile:
 
     # Creating the Redis Search index
     # FT.CREATE songs_index ON JSON SCHEMA $.id AS id TEXT $.name AS name TEXT $.artists AS artists TEXT
-    r.execute_command("FT.DROPINDEX", index_name)
-    r.execute_command("FT.CREATE", index_name, "ON", "JSON", "PREFIX" ,"1", "item:", "SCHEMA", "$.id", "AS", "id", "TEXT", "$.name", "AS", "name", "TEXT", "$.artists", "AS", "artists", "TEXT")
+    # r.execute_command("FT.DROPINDEX", index_name)
+    r.execute_command("FT.CREATE", index_name, "ON", "JSON", "PREFIX" ,"1", "item:", "SCHEMA", "$.id", "AS", "id", "TEXT", "$.name", "AS", "name", "TEXT", "$.artists", "AS", "artists", "TEXT", "$.album", "AS", "album", "TEXT")
     print("Command Executed")
     # Importing data into the created index
     count = 1
     for row in reader:
-        track_id = row['id']
-        track_name = row['name']
+        track_id = row['id'].replace("[", "").replace("]", "").replace("'", "")
+        track_name = row['name'].replace("[", "").replace("]", "").replace("'", "")
         artists = row['artists'].replace("[", "").replace("]", "").replace("'", "")
-        artist_ids = row['artist_ids']
+        artist_ids = row['artist_ids'].replace("[", "").replace("]", "").replace("'", "")
+        album = row['album'].replace("[", "").replace("]", "").replace("'", "")
+        ablum_id = row['album_id'].replace("[", "").replace("]", "").replace("'", "")
+
         # Convert row to JSON format including lists
         json_data = {
             'id': track_id,
             'name': track_name,
             'artists': artists,
-            'artist_ids': artist_ids
+            'artist_ids': artist_ids,
+            'album': album,
+            'album_id': ablum_id
         }
 
         # Add JSON data to the Redis Search index
