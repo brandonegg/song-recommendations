37f3e825b1579f92e47aeecaee13ff5026605002
Brandon Egger <me@brandonegger.com>
2023-12-09 19:54:56 -0600

    add view for when no songs are in curation

--- src/app/components/curation-list.tsx
+++ src/app/components/curation-list.tsx
@@ -4,9 +4,28 @@ import { SongLineItem } from "./song-line-item";
 export default async function CurationList({}: {}) {
   const curationList = await getCurationUsingCookie();
 
+  if (curationList.length === 0) {
+    return (
+      <div className="my-4 mx-auto max-w-[300px] px-4">
+        <div className="mb-4 w-fit mx-auto">
+          <p className="text-white/50 font-semibold text-sm">
+            No songs added yet!
+          </p>
+        </div>
+        <div>
+          <p className="text-white/25 text-center text-sm">
+            <i>
+              Search for a song and select it to start building your curation!
+            </i>
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
-      <div className="pt-8 w-full max-w-[450px] mx-auto space-y-3">
+      <div className="w-full max-w-[450px] mx-auto space-y-3">
         {curationList.map((song, i) => {
           return (
             <SongLineItem {...song} key={i} action="remove-from-curation" />
--- src/app/components/curation-sidebar.tsx
+++ src/app/components/curation-sidebar.tsx
@@ -1,14 +1,22 @@
 import { Suspense } from "react";
 import CurationList from "./curation-list";
 import { LoadingSpinner } from "./loading-spinner";
+import { PlayCircleIcon } from "@heroicons/react/24/solid";
 
 export function CurationSidebar() {
   return (
-    <div className="py-14 px-4 h-full w-full bg-black/20 border-l border-stone-800/50 backdrop-blur-md drop-shadow-left">
-      <h1 className="text-xl font-bold text-white/75">Your Curation:</h1>
-      <Suspense fallback={<LoadingSpinner />}>
-        <CurationList />
-      </Suspense>
+    <div className="h-full w-full bg-black/20 border-l border-stone-800/50 backdrop-blur-md drop-shadow-left">
+      <div className="space-x-2 p-4 bg-black/30 border-b border-white/10 flex flex-row items-center justify-center">
+        <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-center text-xl font-bold text-transparent bg-clip-text">
+          Your Curation
+        </h1>
+        <PlayCircleIcon className="w-6 h-6 text-white/50" />
+      </div>
+      <div className="pt-8 px-4">
+        <Suspense fallback={<LoadingSpinner />}>
+          <CurationList />
+        </Suspense>
+      </div>
     </div>
   );
 }
