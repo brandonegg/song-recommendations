2ed5a408a7b156cb0b427403da5e92e25af0409e
Brandon Egger <me@brandonegger.com>
2023-12-09 18:10:40 -0600

    fix no results UI

--- src/app/components/result-list.tsx
+++ src/app/components/result-list.tsx
@@ -6,15 +6,23 @@ export default async function ResultList({ query }: { query: string }) {
 
   return (
     <>
-      {search.results.length > 0 ? (
+      {query.length > 0 ? (
         <div className="h-[1px] mx-auto w-full max-w-[300px] bg-gray-100/10" />
       ) : null}
 
-      <div className="pt-8 w-full max-w-[450px] mx-auto space-y-3">
-        {search.results.map((song, i) => {
-          return <SongLineItem {...song} key={i} showAddToCuration />;
-        })}
-      </div>
+      {search.results.length == 0 && query.length !== 0 ? (
+        <div className="w-fit mx-auto py-8">
+          <p className="text-white/50 font-semibold text-sm">
+            <i>No results found</i>
+          </p>
+        </div>
+      ) : (
+        <div className="pt-8 w-full max-w-[450px] mx-auto space-y-3">
+          {search.results.map((song, i) => {
+            return <SongLineItem {...song} key={i} showAddToCuration />;
+          })}
+        </div>
+      )}
     </>
   );
 }
